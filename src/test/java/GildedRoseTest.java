5b1398abce6a177d4bb33e1f1d963d475446d9ad
Unknown <iansanleo@gmx.es>
2018-03-12 15:42:16 +0100

    Test: QualityUnder_L

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -77,15 +77,28 @@ public class GildedRoseTest {
 
 		//assert
 		assertTrue(testItem.quality > iniQuality);
-
 	}
+	//The Quality of an item is never more than 50
+	@Test
+	public void QualityUnder_L (){
+		//arrange
+		int topQuality = 50;
+
+		Item testItem = new Item("",2,49);
+		//act
+		GildedRose gR = new GildedRose(testItem);
+		gR.updateQuality();
+
+		//assert
+		assertTrue(testItem.quality < topQuality);
+	}
+
 
 	/*
 
 
 
-    "Aged Brie" actually increases in Quality the older it gets
-    The Quality of an item is never more than 50
+
     "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
     "Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
 
