22b50c2dd697fb43ec1df34e3c00a59e11f41899
Unknown <iansanleo@gmx.es>
2018-03-15 23:21:03 +0100

    Correct TEST

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -128,9 +128,10 @@ public class GildedRoseTest {
 	}
 	//    "Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches;
 	// Quality increases by 2 when there are 10 days or less
+    @Test
     public void tenDaysIncrease_BackstagePasses(){
 	    //arrange
-        String name ="Backstage passes";
+        String name ="Backstage passes to a TAFKAL80ETC concert";
         int iniSellIn = 10;
         int iniQuality = 2;
         int finalQuality = 4;
@@ -142,11 +143,9 @@ public class GildedRoseTest {
 
         //assert
         assertTrue(testItem.getQuality() == finalQuality);
-
     }
-
-
     // and by 3 when there are 5 days or less but
+
 	// Quality drops to 0 after the concert
 
 
