c0b9f82d13456222221cf2dcbdbf682336dccd04
Unknown <iansanleo@gmx.es>
2018-03-16 00:16:33 +0100

    Update processDAy

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -115,12 +115,6 @@ public class GildedRose {
 
             processDay(items.get(i));
 
-            //if(!isLegendary(items.get(i)))
-            //{
-            //    decreaseDay(items.get(i));
-           // }
-
-
 
             if (items.get(i).getSellIn() < 0)
             {
